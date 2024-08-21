db91016212c27c0dde73cd1a767cb41310db7f53
Pansy <zhaowenjia@redrock.team>
2024-08-21 15:34:36 +0800

    修改拖动事件

--- myapp/src/pages/Third/Third.jsx
+++ myapp/src/pages/Third/Third.jsx
@@ -1,72 +1,78 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import './Third.css'
-import { useRef } from "react";
 import src1 from '../../static/images/light.png'
 import src2 from '../../static/images/close.png'
 import src3 from '../../static/images/open.png'
 import src4 from '../../static/images/新ip.png'
 import src5 from '../../static/images/flag1.png'
+import src6 from '../../static/images/left.png'
 import Epp from "../../components/Epp";
-function Third(){
-    const [position,setPosition]=useState({x:430,y:300});
-  const  elementRef=useRef(null);
-  const [isDragging,setIsDragging]=useState(false);
-  const [x,setX]=useState(430);
-  const [y,setY]=useState(300);
-  
-  const [doorSrc,setDoorSrc]=useState(src2)
-    const handleMouseDown=(e)=>{
-        e.preventDefault();
-        setIsDragging(true);
-        setX(e.clientX-elementRef.current.offsetLeft);
-        setY(e.clientY-elementRef.current.offsetTop);
-      }
-      const handleMouseMove=(e)=>{
 
-        if(isDragging){
-          const newPosition={x:e.clientX-x,y:e.clientY-y}
-          setPosition(newPosition)
-          console.log(newPosition)
-          if(newPosition.y <=170){
-            
-             setDoorSrc(src3)
-          }
-          else{
-            setDoorSrc(src2)
-          }
+function Third() {
+    const [position, setPosition] = useState({ x: 430, y: 300 });
+    const elementRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [startY, setStartY] = useState(0);
+
+    const [doorSrc, setDoorSrc] = useState(src2)
+
+    const handleTouchStart = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        setIsDragging(true);
+        const touch = e.touches[0];
+        setStartX(touch.clientX - elementRef.current.offsetLeft);
+        setStartY(touch.clientY - elementRef.current.offsetTop);
+    }
+
+    const handleTouchMove = (e) => {
+        e.preventDefault()
+      e.stopPropagation()
+        if (isDragging) {
+            const touch = e.touches[0];
+            const newPosition = { x: touch.clientX - startX, y: touch.clientY - startY };
+            setPosition(newPosition);
+          
+            if (newPosition.y <= 170) {
+                setDoorSrc(src3);
+            } else {
+                setDoorSrc(src2);
+            }
         }
-        }
-        const handleMouseUp=( )=>{
-   
-          setIsDragging(false);
-        }
+    }
+
+    const handleTouchEnd = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        setIsDragging(false);
+    }
+
     return (
-       <div >
-        <div    
-        
-        className="container">
-            <img
-            className="ip"
-            src={src4} width="100px"/>
-        <img 
-        className="light"
-        src={src1} alt="light"  height="300px" />
-        <img 
-           ref={elementRef}
-           onMouseDown={handleMouseDown}
-           onMouseMove={handleMouseMove}
-           onMouseUp={handleMouseUp}
-           style={{position:"absolute",left:position.x,
-            top:position.y}}
-        className="flag1" src={src5} height="60px"/>
-        <img 
-        width="180px"
-        className="door"
-        src={doorSrc}  />
- 
+        <div >
+            <div    
+                className="container">
+                <img
+                    className="ip"
+                    src={src4} width="100px"/>
+                <img 
+                    className="light"
+                    src={src1} alt="light"  height="300px" />
+                <img 
+                    ref={elementRef}
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                    onTouchEnd={handleTouchEnd}
+                    style={{position:"absolute",left:position.x,
+                        top:position.y}}
+                    className="flag1" src={src5} height="60px"/>
+                <img 
+                    width="180px"
+                    className="door"
+                    src={doorSrc}  />
+            </div>
         </div>
-       </div>
     )
 }
-export default Third
+export default Third;
